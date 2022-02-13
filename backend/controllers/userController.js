af38ec74fe099ae1745d3368d32baaaa8a040437
MohamedBenighil <mohamed.benighil07@gmail.com>
2022-02-13 16:08:19 -0500

    test : Login & Get a  user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,10 +6,25 @@ const registerUser= (req, res) => {
     res.status(200).json({message: 'User registred'})
 }
 
+// @desc    Athenticate a user
+// @route   POST /api/users/login
+// @access  Public
+const loginUser= (req, res) => {
+    res.status(200).json({message: 'User login'})
+}
+
+// @desc    Get user data
+// @route   GET /api/users/me
+// @access  Public
+const getMe= (req, res) => {
+    res.status(200).json({message: 'User data'})
+}
 
 
 
 
 module.exports = {
     registerUser,
+    loginUser,
+    getMe
 }
