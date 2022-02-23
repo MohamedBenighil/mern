3ea349baf1cb7b4759a77fb2c7c9be622d339aae
MohamedBenighil <mohamed.benighil07@gmail.com>
2022-02-23 16:08:17 -0500

    Header & Navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import  {BrowserRouter as Router, Routes, Route} from  'react-router-dom'
+import Header from './components/Header'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
+
 
 function App() {
   return (
     <>
       <Router>
         <div className='container'>
+          <Header />
           <Routes>
             <Route path='/' element={<Dashboard/>} />
             <Route path='/login' element={<Login/>} />
